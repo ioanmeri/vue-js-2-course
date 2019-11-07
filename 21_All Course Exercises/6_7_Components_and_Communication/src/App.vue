1f26259a4b8fe6cb827d20b3dd6ece279272bdf7
ioanmeri <ioannismeri@gmail.com>
2019-11-07 14:38:17 +0200

    5,6,7

---
+++ 21_All Course Exercises/6_7_Components_and_Communication/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <app-servers></app-servers>
+      <app-server-details></app-server-details>
+    </div>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Shared/Header.vue';
+  import Footer from './components/Shared/Footer.vue';
+  import Servers from './components/Server/Servers.vue';
+  import ServerDetails from './components/Server/ServerDetails.vue';
+
+  export default{
+    components: {
+      appHeader: Header,
+      appFooter: Footer,
+      appServers: Servers,
+      appServerDetails: ServerDetails,
+    }
+  }
+</script>
+
+<style>
+  
+</style>
